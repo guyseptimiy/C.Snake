5d8ee2838abc56dc14c906a5769d102f24e2dbed
Валентин Виноградов <guy_septimiy@mail.ru>
2022-06-07 18:38:43 +0300

    классы горизонтальной и вертикальной линии

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -12,33 +12,15 @@ namespace snake
         {
             Point p1 = new Point(1, 3, '*');
             Point p2 = new Point(4, 5, '#');
-            Point p3 = new Point(10, 12, '%');
-            Point p4 = new Point(12, 10, '&');
 
             p1.Draw();
             p2.Draw();
-            p3.Draw();
-            p4.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine HL = new HorizontalLine(2, 40, 7, '#');
+            HL.Draw();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            VerticalLine VL = new VerticalLine(2, 7, 20, '#');
+            VL.Draw();
 
             Console.ReadLine();
         }
