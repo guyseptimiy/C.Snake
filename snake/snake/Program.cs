76565bf033fd28940b6924837c3e1d102cd7f55f
Валентин Виноградов <guy_septimiy@mail.ru>
2022-06-07 18:06:24 +0300

    списки

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -11,10 +11,34 @@ namespace snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
             Point p2 = new Point(4, 5, '#');
+            Point p3 = new Point(10, 12, '%');
+            Point p4 = new Point(12, 10, '&');
+
+            p1.Draw();
             p2.Draw();
+            p3.Draw();
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
         }
